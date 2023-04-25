5b753904baee1ea18b3eae8f2807a943f5aa1cbc
xanmankey <86136010+xanmankey@users.noreply.github.com>
2023-04-25 18:14:11 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# scholarship_finder
+A simple scholarship finder that utilizes web scraping and web automation to retrieve a list of urls to scholarships with desired keywords and criteria
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scholarship_finder',
+    version='0.1.0',
+    description='A simple scholarship finder that utilizes web scraping and web automation to retrieve a list of urls to scholarships with desired keywords and criteria',
+    author='Xander Kutulas, Nathon whose last name I dont know lol',
+    author_email='myEmail, nathans email',
+    url='https://github.com/xanmankey/scholarship_finder.git',
+    packages=find_packages(),
+)
