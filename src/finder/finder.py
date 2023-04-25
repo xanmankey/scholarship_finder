b8f010d7d6f8ce9446b24d23c12375b3785aa270
xanmankey <86136010+xanmankey@users.noreply.github.com>
2023-04-25 18:46:48 -0500

    laying out finder.py

---
+++ src/finder/finder.py
@@ -0,0 +1,38 @@
+### GOALS
+## Scholarship identifier not limited to one platform
+## Comprehensive keyword identification (value, major, location, ethnicity, etc)
+
+# Utilizes command line arguments
+import sys 
+
+args = sys.argv 
+
+# TODO: implement finder class
+class Finder(): 
+    """A class for managing web scraping and automation"""
+    def __init__(self): 
+        return NotImplementedError
+
+
+# TODO: implement identifier class
+class Identifier(): 
+    """A class for identifying scholarships based on keywords
+    Common keyword identifiers are:
+    - Value
+    - Major
+    - Location
+    - Discipline
+    - Race 
+    - Ethnicity
+    - TODO
+    """
+    def __init__(self): 
+        return NotImplementedError
+    
+if __name__ == "__main__": 
+    # TODO: Create instance of Finder class
+
+    # TODO: Assign correct identifiers
+
+    # TODO: Begin the scrape and automation process
+    NotImplementedError
